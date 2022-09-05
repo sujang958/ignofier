ada4546328ec5f6e991fd216bd321646489b7ed4
sujang958 <enter7377@naver.com>
2022-09-05 23:07:06 +0900

    :sparkles:  Add checking version

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { join } from "path"
 import { ask, createFile } from "./creation"
 import { setup } from "./git"
 
+;(["-v", "--version"].includes(process.argv[2]) &&
+  console.log(require("../package.json").version)) ||
+  process.exit()
+
 const run = async () => {
   setup()
   const { selected: chosenFile } = await ask()
